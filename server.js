377fb76bb9f54bc7fcad584479b82f9b89e60769
aginamena5 <aginamena5@gmail.com>
2022-06-01 22:46:49 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const rooms = ["general", "tech", "finance", "crypto"]
+// we want the client to able to access the server
+const cors = require("cors");
+app.use(cors());
+
+//we need express.urlencoded and express.json for POSTS and PUT requests
+//we are telling the server to accept the object sent from the from the client
+//which is stored in the body
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+
+const server = require("http").createServer(app)
+const port = 5000
+const io = require("socket.io")(server, {
+    cors: {
+        // where the client is located
+        origin: "http://localhost:3000",
+        methods: ["Get", "Post"]
+    }
+})
+
+server.listen(port, () => console.log("Server is running on part " + port))
